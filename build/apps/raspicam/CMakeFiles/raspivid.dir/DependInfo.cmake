29abff57ae6f83d911071b7fb45f0fe013aef840
Olaf Schulz <funwithkinect@googlemail.com>
2014-05-15 11:51:29 +0000

    Add raspivid app as subproject. Use Licence from official userland repo for this subproject

---
+++ build/apps/raspicam/CMakeFiles/raspivid.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/olaf/software/camera/RPIMotionDetection/apps/raspicam/RaspiCLI.c" "/home/olaf/software/camera/RPIMotionDetection/build/apps/raspicam/CMakeFiles/raspivid.dir/RaspiCLI.c.o"
+  "/home/olaf/software/camera/RPIMotionDetection/apps/raspicam/RaspiCamControl.c" "/home/olaf/software/camera/RPIMotionDetection/build/apps/raspicam/CMakeFiles/raspivid.dir/RaspiCamControl.c.o"
+  "/home/olaf/software/camera/RPIMotionDetection/apps/raspicam/RaspiPreview.c" "/home/olaf/software/camera/RPIMotionDetection/build/apps/raspicam/CMakeFiles/raspivid.dir/RaspiPreview.c.o"
+  "/home/olaf/software/camera/RPIMotionDetection/apps/raspicam/RaspiVid.c" "/home/olaf/software/camera/RPIMotionDetection/build/apps/raspicam/CMakeFiles/raspivid.dir/RaspiVid.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "../BlobDetection"
+  "/opt/vc/include"
+  "/opt/vc/include/interface/vcos"
+  "/opt/vc/include/interface/vcos/pthreads"
+  "/opt/vc/include/interface/vmcs_host/linux"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/apps/raspicam/CMakeFiles/raspivid.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/raspivid.dir/RaspiCamControl.c.o"
+  "CMakeFiles/raspivid.dir/RaspiCLI.c.o"
+  "CMakeFiles/raspivid.dir/RaspiPreview.c.o"
+  "CMakeFiles/raspivid.dir/RaspiVid.c.o"
+  "raspivid.pdb"
+  "raspivid"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/raspivid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
