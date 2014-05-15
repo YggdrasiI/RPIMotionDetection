70f8fd4d3cf24ea5bff3d22b5f97db41313d4f41
Olaf Schulz <funwithkinect@googlemail.com>
2014-05-15 18:04:27 +0000

    Add gl output to raspivid

---
+++ apps/raspicam/RaspiVid.h
@@ -0,0 +1,16 @@
+#ifndef RASPVID_H
+#define RASPVID_H
+
+#ifdef __cplusplus
+extern "C" 
+{
+#endif
+
+int raspivid(int argc, const char **argv);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ apps/raspicam/main.cpp
@@ -0,0 +1,13 @@
+#include "RaspiVid.h"
+
+int main(int argc, const char **argv){
+
+	//Create thread for blob detection.
+	
+	//Add arguments to force opengl output of 
+	//raspivid
+
+	//start raspivid application.
+	raspivid(argc, argv);
+
+}
