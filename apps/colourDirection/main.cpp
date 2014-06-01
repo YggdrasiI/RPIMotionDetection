3ae6b00f92be52856a83666e6e9222ccb3732c8a
Olaf Schulz <funwithkinect@googlemail.com>
2014-06-01 16:15:09 +0000

    Add stubs for the apps 'gestures' and 'colourDirections'

---
+++ apps/colourDirection/main.cpp
@@ -0,0 +1,162 @@
+#include <pthread.h>
+
+#include "RaspiVid.h"
+#include "RaspiImv.h"
+
+#include "depthtree.h"
+#include "Tracker2.h"
+#include "Graphics.h"
+
+static DepthtreeWorkspace *dworkspace = NULL;
+static Blobtree *frameblobs = NULL;
+Tracker2 tracker;
+unsigned char depth_map[256];
+pthread_t blob_tid;
+
+static void eval_ids(DepthtreeWorkspace *dworkspace, unsigned char *out, int len ){
+	unsigned int *ids, *cm, *cs;
+	ids = dworkspace->ids;
+	cm = dworkspace->comp_same;
+	cs = dworkspace->comp_size;
+	while(--len){
+		if( *(cs + *ids) > 8 ){
+			*out = *(cm + *ids);
+		}else{
+			*out = 1;
+		}
+		++out;++ids;
+	}
+}
+
+void* blob_detection(void *argn){
+
+      while (1){
+				if( motion_data.available ){
+
+					//swap pointers if possible
+					while(motion_data.mutex){
+						usleep(1000);
+					}
+					motion_data.mutex = 1;
+					char *tmp = motion_data.imv_array_in;
+					motion_data.imv_array_in = motion_data.imv_array_buffer;
+					motion_data.imv_array_buffer = tmp;
+					motion_data.available = 0;
+					motion_data.mutex = 0;
+
+					//1. Convert imv vector to norm.
+					//Note: Some uness. operations if gridwidth>1.
+					//imv_eval_norm(&motion_data);
+					imv_eval_norm2(&motion_data);
+
+					//1.5 (optional) OpenGl Output
+					if( true ){
+						/* Problem: This texture update is outside
+						 * of the gl context thread and will be
+						 * ignored. Solution?!"
+						 * */
+						//imvTexture.SetPixels(motion_data.imv_norm);
+					}
+
+					//2. Blob detection
+					BlobtreeRect input_roi = {0,0, motion_data.width, motion_data.height -0 };//shrink height because lowest rows contains noise.
+					depthtree_find_blobs(frameblobs, motion_data.imv_norm,
+							motion_data.width, motion_data.height,
+							input_roi, depth_map, dworkspace);
+
+					//3. Tracker
+					tracker.trackBlobs( frameblobs, true );
+					
+					//4. Opengl Output
+					// see gl_scenes/motion.c
+
+					// Debug: Replace imv_norm with ids, roi has to start in (0,0) and with full width.
+					eval_ids(dworkspace, motion_data.imv_norm, input_roi.width* input_roi.height);
+
+
+				}else{
+					//printf("No new imv data\n");
+					//vcos_sleep(100);
+					usleep(100000);
+				}
+			}
+}
+
+
+static int hand_filter(Node *n){
+
+	const Blob* const data = (Blob*) n->data;
+
+	/* Remove Blobs which has almost the same size as their (first) child node */
+	if( n->child != NULL ){
+		const Blob* const cdata = (Blob*) n->child->data;
+		if( ((float)(cdata->area)) > (data->area) * 0.99f ){
+			return 1;
+		}
+	}
+
+	//remove big areas
+	if( 10 *( data->roi.width * data->roi.height )>120*68 )
+		return 1;
+
+	// remove areas which are very unsquared
+	if( data->area > 25
+			&& (	data->roi.width > 3*data->roi.height 
+				|| data->roi.height > 3*data->roi.width ) )
+	{
+		return 1;
+	}
+	
+	// remove areas with sparse content
+	if( data->roi.width + data->roi.height > 4*data->area ){
+		return 1;
+	}
+
+	return 0;
+}
+
+int main(int argc, const char **argv){
+
+	// Create objects for blob detection and tracker
+	// 120x68 for 1080p encoding resolution.
+	depthtree_create_workspace( 120, 68, &dworkspace );
+	if( dworkspace == NULL ){
+		printf("Unable to create workspace.\n");
+		return -1;
+	}
+	blobtree_create(&frameblobs);
+	blobtree_set_grid(frameblobs, 1, 1);
+
+	blobtree_set_filter(frameblobs, F_AREA_MIN, 75 );
+	blobtree_set_filter(frameblobs, F_AREA_MAX, 1000 );
+	blobtree_set_extra_filter(frameblobs, hand_filter);
+	/* Only show leafs with above filtering effects */
+	blobtree_set_filter(frameblobs, F_ONLY_LEAFS, 1);
+
+	//init depth map
+	for( int i=0; i<256; i++){
+		depth_map[i] = (i<10?0:i/4+1);
+	}
+	//Create thread for blob detection.
+	int err = pthread_create(&blob_tid, NULL, &blob_detection, NULL);
+	if (err != 0){
+		printf("\nFailed to create blob thread :[%s]", strerror(err));
+		return -1;
+	}
+
+	/* Setup tracker */
+	tracker.setMaxRadius(15);
+	//filter out blobs with live time < M frames
+	tracker.setMinimalDurationFilter(5);
+	//reduce output to N oldest blobs
+	tracker.setOldestDurationFilter(2);
+
+	//start raspivid application.
+	raspivid(argc, argv);
+
+
+	depthtree_destroy_workspace( &dworkspace );
+	blobtree_destroy(&frameblobs);
+}
+
+
---
+++ apps/gestures/Graphics.cpp
@@ -0,0 +1,231 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <unistd.h>
+#include <iostream>
+
+#ifdef WITH_OCV
+#include <opencv2/opencv.hpp>
+#endif
+
+#include "bcm_host.h"
+#include "RaspiImv.h"
+
+#include "Graphics.h"
+#include "DrawingFunctions.h"
+
+#include "Tracker2.h"
+extern Tracker2 tracker;
+
+#define check() assert(glGetError() == 0)
+
+
+//List of Gfx*Objects which will be used in this app.
+extern uint32_t GScreenWidth;
+extern uint32_t GScreenHeight;
+extern EGLDisplay GDisplay;
+extern EGLSurface GSurface;
+extern EGLContext GContext;
+
+extern GfxShader GSimpleVS;
+extern GfxShader GSimpleFS;
+extern GfxShader GBlobFS;
+extern GfxShader GBlobsVS;
+extern GfxShader GBlobsFS;
+extern GfxShader GColouredLinesFS;
+
+extern GfxProgram GSimpleProg;
+extern GfxProgram GBlobProg;
+extern GfxProgram GBlobsProg;
+extern GfxProgram GColouredLinesProg;
+
+extern GLuint GQuadVertexBuffer;
+
+GfxTexture imvTexture;
+static std::vector<cBlob> blobCache;
+
+
+
+void InitGraphics()
+{
+	bcm_host_init();
+	int32_t success = 0;
+	EGLBoolean result;
+	EGLint num_config;
+
+	static EGL_DISPMANX_WINDOW_T nativewindow;
+
+	DISPMANX_ELEMENT_HANDLE_T dispman_element;
+	DISPMANX_DISPLAY_HANDLE_T dispman_display;
+	DISPMANX_UPDATE_HANDLE_T dispman_update;
+	VC_RECT_T dst_rect;
+	VC_RECT_T src_rect;
+
+	static const EGLint attribute_list[] =
+	{
+		EGL_RED_SIZE, 8,
+		EGL_GREEN_SIZE, 8,
+		EGL_BLUE_SIZE, 8,
+		EGL_ALPHA_SIZE, 8,
+		EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+		EGL_NONE
+	};
+
+	static const EGLint context_attributes[] = 
+	{
+		EGL_CONTEXT_CLIENT_VERSION, 2,
+		EGL_NONE
+	};
+	EGLConfig config;
+
+	// get an EGL display connection
+	GDisplay = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+	assert(GDisplay!=EGL_NO_DISPLAY);
+	check();
+
+	// initialize the EGL display connection
+	result = eglInitialize(GDisplay, NULL, NULL);
+	assert(EGL_FALSE != result);
+	check();
+
+	// get an appropriate EGL frame buffer configuration
+	result = eglChooseConfig(GDisplay, attribute_list, &config, 1, &num_config);
+	assert(EGL_FALSE != result);
+	check();
+
+	// get an appropriate EGL frame buffer configuration
+	result = eglBindAPI(EGL_OPENGL_ES_API);
+	assert(EGL_FALSE != result);
+	check();
+
+	// create an EGL rendering context
+	GContext = eglCreateContext(GDisplay, config, EGL_NO_CONTEXT, context_attributes);
+	assert(GContext!=EGL_NO_CONTEXT);
+	check();
+
+	// create an EGL window surface
+	success = graphics_get_display_size(0 /* LCD */, &GScreenWidth, &GScreenHeight);
+	assert( success >= 0 );
+
+	dst_rect.x = 0;
+	dst_rect.y = 0;
+	dst_rect.width = GScreenWidth;
+	dst_rect.height = GScreenHeight;
+
+	src_rect.x = 0;
+	src_rect.y = 0;
+	src_rect.width = GScreenWidth << 16;
+	src_rect.height = GScreenHeight << 16;        
+
+	dispman_display = vc_dispmanx_display_open( 0 /* LCD */);
+	dispman_update = vc_dispmanx_update_start( 0 );
+
+	dispman_element = vc_dispmanx_element_add ( dispman_update, dispman_display,
+		0/*layer*/, &dst_rect, 0/*src*/,
+		&src_rect, DISPMANX_PROTECTION_NONE, 0 /*alpha*/, 0/*clamp*/, (DISPMANX_TRANSFORM_T)0/*transform*/);
+
+	nativewindow.element = dispman_element;
+	nativewindow.width = GScreenWidth;
+	nativewindow.height = GScreenHeight;
+	vc_dispmanx_update_submit_sync( dispman_update );
+
+	check();
+
+	GSurface = eglCreateWindowSurface( GDisplay, config, &nativewindow, NULL );
+	assert(GSurface != EGL_NO_SURFACE);
+	check();
+
+	// connect the context to the surface
+	result = eglMakeCurrent(GDisplay, GSurface, GSurface, GContext);
+	assert(EGL_FALSE != result);
+	check();
+
+	// Set background color and clear buffers
+	glClearColor(0.15f, 0.25f, 0.35f, 1.0f);
+	glClear( GL_COLOR_BUFFER_BIT );
+
+	InitShaders();
+}
+
+void InitTextures(uint32_t glWinWidth, uint32_t glWinHeight)
+{
+	/* Begin of row values is NOT word-aligned. Set alignment to 1 */
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); 
+	imvTexture.CreateGreyScale(121,68);
+	//imvTexture.GenerateFrameBuffer();
+}
+
+void RedrawGui()
+{
+	// no gui in this app
+}
+
+
+void RedrawTextures()
+{
+
+	imvTexture.SetPixels(motion_data.imv_norm);
+	//DrawTextureRect(&imvTexture,-1.0, -1.f,-1.f,1.f,1.f,NULL);
+	//Use Scaling to flip horizontal
+	//DrawTextureRect(&imvTexture,-1.0, .5f,-.5f,-.5f,.5f,NULL);
+	DrawTextureRect(&imvTexture,0.4, 1.0f,-1.0f,-1.0f,1.0f,NULL);
+
+	blobCache.clear();
+	tracker.getFilteredBlobs(TRACK_ALL_ACTIVE|LIMIT_ON_N_OLDEST, blobCache);
+	tracker.drawBlobsGL(motion_data.width, motion_data.height, true, &blobCache);
+	//tracker.drawBlobsGL(motion_data.width, motion_data.height);
+
+#if 0
+	static int savecounter=0;
+	if( savecounter == 400 )
+		SaveFrameBuffer("/dev/shm/fb1.png");
+	++savecounter;
+#endif
+}
+
+void InitShaders()
+{
+	//load shaders
+	GSimpleVS.LoadVertexShader("shader/simplevertshader.glsl");
+	//GSimpleFS.LoadFragmentShader("shader/simplefragshader.glsl");
+	GSimpleFS.LoadFragmentShader("shader/blobids_fragshader.glsl");
+	GSimpleProg.Create(&GSimpleVS,&GSimpleFS);
+	check();
+	GBlobFS.LoadFragmentShader("shader/blobfragshader.glsl");
+	GBlobProg.Create(&GSimpleVS,&GBlobFS);
+	GBlobsVS.LoadVertexShader("shader/blobsvertshader.glsl");
+	GBlobsFS.LoadFragmentShader("shader/blobsfragshader.glsl");
+	GBlobsProg.Create(&GBlobsVS,&GBlobsFS);
+
+	GColouredLinesFS.LoadFragmentShader("shader/colouredlinesfragshader.glsl");
+	GColouredLinesProg.Create(&GBlobsVS,&GColouredLinesFS);
+
+	check();
+
+	//create an ickle vertex buffer
+	static const GLfloat quad_vertex_positions[] = {
+		0.0f, 0.0f,	1.0f, 1.0f,
+		1.0f, 0.0f, 1.0f, 1.0f,
+		0.0f, 1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f, 1.0f
+	};
+	glGenBuffers(1, &GQuadVertexBuffer);
+	check();
+	glBindBuffer(GL_ARRAY_BUFFER, GQuadVertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(quad_vertex_positions), quad_vertex_positions, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	check();
+}
+
+void ReleaseGraphics()
+{
+
+}
+
+int GetShader(){
+	return ShaderNormal;
+}
+
